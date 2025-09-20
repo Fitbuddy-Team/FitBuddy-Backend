cc430ea4ca867abb2e80201aa881aacc38817faa
mane6867 <magdalena.pino@uc.cl>
2025-09-19 21:55:18 -0300

    feat: add getExercisesByMuscleGroup endpoint and muscle group controller

--- src/app/controllers/exercise.controller.js
+++ src/app/controllers/exercise.controller.js
@@ -1,4 +1,5 @@
 import { Exercise } from '../models/index.js';
+import { MuscleGroup } from '../models/index.js';
 
 export const exerciseController = {
   getAllExercises: (req, res) => {
@@ -10,6 +11,22 @@ export const exerciseController = {
   getExercisesByCategory: (req, res) => {
     const { catId } = req.params;
     Exercise.findAll({ where: { categoryId: catId } })
+      .then(exercises => res.json({ exercises }))
+      .catch(err => res.status(500).json({ error: err.message }));
+  },
+
+  getExercisesByMuscleGroup: (req, res) => {
+    const { groupId } = req.params;
+    Exercise.findAll({
+      include: [{
+        model: MuscleGroup,
+        as: 'muscles',
+        where: { id: groupId },
+        through: { attributes: [] },
+        required: true 
+      }
+      ]
+    })
       .then(exercises => res.json({ exercises }))
       .catch(err => res.status(500).json({ error: err.message }));
   }
--- src/app/routes/index.js
+++ src/app/routes/index.js
@@ -1,13 +1,20 @@
 import { Router } from 'express';
 import { healthController } from '../controllers/health.controller.js';
-import {exerciseController} from '../controllers/exercise.controller.js';
+import { exerciseController } from '../controllers/exercise.controller.js';
+import  {muscleGroupController } from '../controllers/musclegroups.controller.js';
 
 const router = Router();
 
 
 router.get('/health', healthController.health);
+
+// Exercises
 router.get('/exercises', exerciseController.getAllExercises);
 router.get('/exercises/category/:catId', exerciseController.getExercisesByCategory);
+router.get('/exercises/musclegroup/:groupId', exerciseController.getExercisesByMuscleGroup);
+
+//Muscle Groups
+router.get('/musclegroups', muscleGroupController.getAllMuscleGroups);
 
 export default router;
 
