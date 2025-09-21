7d5f0d6a2538482a2eaa70fcb0b0f0a9cecc2cb5
Dominique236 <dominique.soto@uc.cl>
2025-09-21 15:02:58 -0300

    fix: user associations

--- src/app/models/index.js
+++ src/app/models/index.js
@@ -29,27 +29,31 @@ const sequelize = new Sequelize(
 );
 
 // Inicializar modelos
-const models = {
-  Exercise: ExerciseModel(sequelize, DataTypes),
-  ExerciseCategory: ExerciseCategoryModel(sequelize, DataTypes),
-  ExerciseMuscleGroup: ExerciseMuscleGroupModel(sequelize, DataTypes),
-  MuscleGroup: MuscleGroupModel(sequelize, DataTypes),
-  Routine: RoutineModel(sequelize, DataTypes),
-  Session: SessionModel(sequelize, DataTypes),
-  Set: SetModel(sequelize, DataTypes),
-  User: UserModel(sequelize, DataTypes),
-  ExerciseRoutine: ExerciseRoutineModel(sequelize, DataTypes),
-  ExerciseSession: ExerciseSessionModel(sequelize, DataTypes),
-};
+const Exercise = ExerciseModel(sequelize, DataTypes);
+const ExerciseCategory = ExerciseCategoryModel(sequelize, DataTypes);
+const ExerciseMuscleGroup = ExerciseMuscleGroupModel(sequelize, DataTypes);
+const MuscleGroup = MuscleGroupModel(sequelize, DataTypes);
+const Routine = RoutineModel(sequelize, DataTypes);
+const Session = SessionModel(sequelize, DataTypes);
+const Set = SetModel(sequelize, DataTypes);
+const User = UserModel(sequelize, DataTypes);
+const ExerciseRoutine = ExerciseRoutineModel(sequelize, DataTypes);
+const ExerciseSession = ExerciseSessionModel(sequelize, DataTypes);
 
 // Asociaciones
-Object.values(models).forEach(model => {
-  if (model.associate) {
-    model.associate(models);
-  }
-});
+Exercise.associate?.({ ExerciseCategory, User, MuscleGroup, Routine, ExerciseMuscleGroup, ExerciseRoutine });
+ExerciseCategory.associate?.({ Exercise });
+ExerciseMuscleGroup.associate?.({ Exercise, MuscleGroup });
+MuscleGroup.associate?.({ Exercise, ExerciseMuscleGroup });
+Routine.associate?.({ User, Exercise, ExerciseRoutine });
+Session.associate?.({ User, Routine, Exercise, ExerciseSession });
+Set.associate?.({ ExerciseRoutine, ExerciseSession });
+User.associate?.({ Routine, Session, Exercise });
+ExerciseRoutine.associate?.({ Routine, Exercise, Set });
+ExerciseSession.associate?.({ Session, Exercise, Set });
 
-export const {
+export {
+  sequelize,
   Exercise,
   ExerciseCategory,
   ExerciseMuscleGroup,
@@ -60,6 +64,4 @@ export const {
   User,
   ExerciseRoutine,
   ExerciseSession
-} = models;
-
-export { sequelize };
+};
